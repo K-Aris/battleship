552e3f5336b882388e34015cf6ab7f820fffdf17
Kurt Aris <kurtarisohn@gmail.com>
2019-09-18 15:47:28 -0700

    Update hit/miss function

--- battleship.js
+++ battleship.js
@@ -3,10 +3,20 @@ var view = {
         var messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg; 
     }, 
+
     displayHit: function(location) {
-
+        var cell = document.getElementById(location); 
+        cell.setAttribute("class", "hit"); 
     },
-    displayMiss: function(location) {
 
+    displayMiss: function(location) {
+        var cell = document.getElementById(location); 
+        cell.setAttribute("class", "miss"); 
     }
 }; 
+
+view.displayMiss("00"); 
+view.displayHit("06"); 
+view.displayMiss("31"); 
+
+view.displayMessage("Is this thing on?"); 
